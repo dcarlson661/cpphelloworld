096cdd6aa62f8a7ca6e91ea20ff3354d872c3a7c
dcarl661 <dcarl@earthlink.net>
2021-05-19 16:04:46 -0700

    simple iterators and find algorithms

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -19,6 +21,82 @@ void func2(int *y)
 
 int main()
 {
+	string str("hello");
+	string::iterator it = str.begin();
+	while (it != str.end())
+	{
+		cout << *it << ",";
+		++it;
+	}
+	cout << endl;
+	for (string::iterator it = str.begin(); it != str.end(); ++it)
+	{
+		cout << *it << ",";
+	}
+	cout << endl;
+	for (string::reverse_iterator it = str.rbegin(); it != str.rend(); ++it)
+	{
+		cout << *it << ",";
+	}
+	cout << endl;
+	for (string::const_iterator it = str.begin(); it != str.end(); ++it)
+	{
+		cout << *it << ",";
+	}
+	string::iterator second = str.begin() + 1;
+	cout << endl << *second << endl;
+	// end() - begin() gives the number of elements
+	string::iterator mid = str.begin() + (str.end() - str.begin()) / 2;  // Iterator to middle element
+	cout << "Middle element is " << *mid << endl;
+
+	string::iterator last = str.end() - 1;  // Iterator to last element
+	cout << "Last element is " << *last << endl;
+
+	const char strb[] = { 'H','e','l','l','o' };
+	const char *pEnd = strb + 5;
+	const char *p = strb;
+	while (p != pEnd)
+	{
+		cout << *p << ",";
+		++p;
+	}
+
+	string strc("hello world");
+	char c = 'l';
+	cout << "the string is " << strc << endl;
+	cout << "searching for char " << c << endl;
+	string::iterator res = find(strc.begin(), strc.end(), c);
+	if (res != strc.end())
+	{
+		cout << "found match " << res - strc.begin() << endl;
+	}
+	else
+	{
+		cout << "not found" << c << endl;
+	}
+}
+int mainVec()
+{
+	vector<int> vec;
+	vec.push_back(4);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	vec.push_back(1);
+
+	cout << "E0 " << vec[0] << endl;
+	cout << "E2 " << vec[2] << endl;
+	cout << "E4 " << vec[4] << endl;
+
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << "E" << i << "=" << vec[i] << endl;
+	}
+	return 0;
+}
+
+int mainLessonOne()
+{
     std::cout << "Hello World!\n";
 	int x = 2;
 	cout << "address of x is " << &x << endl;
@@ -30,8 +108,7 @@ int main()
 	cout << "address of x is " << &x << endl;
 	func2(&x);
 	cout << "after calling func2(int *y), x = " << &x << endl;
-
-
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
